8fff16dca88699b43dccfa75e59e95d3cfbb7c56
Mathias Bynens <mathias@qiwi.be>
2021-07-30 12:44:52 +0200

    Modernize code a bit

--- scripts/export-data.js
+++ scripts/export-data.js
@@ -5,6 +5,14 @@ function readJSON(path) {
 	return JSON.parse(fs.readFileSync(path, 'utf-8'));
 }
 
+function objectToMap(object) {
+	return new Map(
+		Object.entries(object).map(([ key, value ]) => {
+			return [ Number(key), value ];
+		})
+	);
+}
+
 module.exports = {
 	labels: jsesc(readJSON('data/labels.json'), {
 		compact: false,
@@ -12,7 +20,7 @@ module.exports = {
 	}),
 	encoded: jsesc(readJSON('data/encoded.json'), { wrap: true }),
 	decoded: jsesc(readJSON('data/decoded.json'), { wrap: true }),
-	indexByCodePoint: jsesc(readJSON('data/index-by-code-point.json')),
-	indexByPointer: jsesc(readJSON('data/index-by-pointer.json')),
+	indexByCodePoint: jsesc(objectToMap(readJSON('data/index-by-code-point.json'))),
+	indexByPointer: jsesc(objectToMap(readJSON('data/index-by-pointer.json'))),
 	version: readJSON('package.json').version
 };
--- src/windows-1252.src.js
+++ src/windows-1252.src.js
@@ -1,19 +1,17 @@
 /*! https://mths.be/windows-1252 v<%= version %> by @mathias | MIT license */
 ;(function() {
 
-	var object = {};
-	var hasOwnProperty = object.hasOwnProperty;
-	var stringFromCharCode = String.fromCharCode;
+	const stringFromCharCode = String.fromCharCode;
 
-	var INDEX_BY_CODE_POINT = <%= indexByCodePoint %>;
-	var INDEX_BY_POINTER = <%= indexByPointer %>;
+	const INDEX_BY_CODE_POINT = <%= indexByCodePoint %>;
+	const INDEX_BY_POINTER = <%= indexByPointer %>;
 
 	// https://encoding.spec.whatwg.org/#error-mode
-	var error = function(codePoint, mode) {
+	const error = (codePoint, mode) => {
 		if (mode == 'replacement') {
 			return '\uFFFD';
 		}
-		if (codePoint != null && mode == 'html') {
+		if (codePoint !== null && mode === 'html') {
 			return '&#' + codePoint + ';';
 		}
 		// Else, `mode == 'fatal'`.
@@ -21,23 +19,19 @@
 	};
 
 	// https://encoding.spec.whatwg.org/#single-byte-decoder
-	var decode = function(input, options) {
-		var mode;
+	const decode = (input, options) => {
+		let mode;
 		if (options && options.mode) {
 			mode = options.mode.toLowerCase();
 		}
 		// “An error mode […] is either `replacement` (default) or `fatal` for a
 		// decoder.”
-		if (mode != 'replacement' && mode != 'fatal') {
+		if (mode !== 'replacement' && mode !== 'fatal') {
 			mode = 'replacement';
 		}
-		var length = input.length;
-		var index = -1;
-		var byteValue;
-		var pointer;
-		var result = '';
-		while (++index < length) {
-			byteValue = input.charCodeAt(index);
+		let result = '';
+		for (let index = 0; index < input.length; index++) {
+			const byteValue = input.charCodeAt(index);
 			// “If `byte` is in the range `0x00` to `0x7F`, return a code point whose
 			// value is `byte`.”
 			if (byteValue >= 0x00 && byteValue <= 0x7F) {
@@ -46,10 +40,10 @@
 			}
 			// “Let `code point` be the index code point for `byte − 0x80` in index
 			// `single-byte`.”
-			pointer = byteValue - 0x80;
-			if (hasOwnProperty.call(INDEX_BY_POINTER, pointer)) {
+			const pointer = byteValue - 0x80;
+			if (INDEX_BY_POINTER.has(pointer)) {
 				// “Return a code point whose value is `code point`.”
-				result += INDEX_BY_POINTER[pointer];
+				result += INDEX_BY_POINTER.get(pointer);
 			} else {
 				// “If `code point` is `null`, return `error`.”
 				result += error(null, mode);
@@ -59,23 +53,19 @@
 	};
 
 	// https://encoding.spec.whatwg.org/#single-byte-encoder
-	var encode = function(input, options) {
-		var mode;
+	const encode = (input, options) => {
+		let mode;
 		if (options && options.mode) {
 			mode = options.mode.toLowerCase();
 		}
 		// “An error mode […] is either `fatal` (default) or `HTML` for an
 		// encoder.”
-		if (mode != 'fatal' && mode != 'html') {
+		if (mode !== 'fatal' && mode !== 'html') {
 			mode = 'fatal';
 		}
-		var length = input.length;
-		var index = -1;
-		var codePoint;
-		var pointer;
-		var result = '';
-		while (++index < length) {
-			codePoint = input.charCodeAt(index);
+		let result = '';
+		for (let index = 0; index < input.length; index++) {
+			const codePoint = input.charCodeAt(index);
 			// “If `code point` is in the range U+0000 to U+007F, return a byte whose
 			// value is `code point`.”
 			if (codePoint >= 0x00 && codePoint <= 0x7F) {
@@ -84,8 +74,8 @@
 			}
 			// “Let `pointer` be the index pointer for `code point` in index
 			// `single-byte`.”
-			if (hasOwnProperty.call(INDEX_BY_CODE_POINT, codePoint)) {
-				pointer = INDEX_BY_CODE_POINT[codePoint];
+			if (INDEX_BY_CODE_POINT.has(codePoint)) {
+				const pointer = INDEX_BY_CODE_POINT.get(codePoint);
 				// “Return a byte whose value is `pointer + 0x80`.”
 				result += stringFromCharCode(pointer + 0x80);
 			} else {
@@ -96,7 +86,7 @@
 		return result;
 	};
 
-	var windows1252 = {
+	const windows1252 = {
 		encode: encode,
 		decode: decode,
 		labels: <%= labels %>,
