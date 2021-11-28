a3710c0760e0b74173e4ce7fbf08c8af164e2a57
Mathias Bynens <mathias@qiwi.be>
2021-11-28 12:47:44 -0300

    Add TypeScript typings
    Issue: https://github.com/mathiasbynens/windows-1252/issues/6

---
+++ src/windows-1252.d.ts
@@ -0,0 +1,18 @@
+declare module 'windows-1252' {
+  type EncodeOptions = {
+    mode: 'fatal' | 'replacement';
+  };
+  type DecodeOptions = {
+    mode: 'fatal' | 'replacement';
+  };
+
+  export declare function encode(
+    text: string,
+    options?: EncodeOptions
+  ): Uint16Array;
+  export declare function decode(
+    buffer: Uint16Array | Uint8Array | Buffer | string,
+    options?: DecodeOptions
+  ): string;
+  export type labels = string[];
+}
